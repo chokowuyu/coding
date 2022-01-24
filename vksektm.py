2d37f78b09d2d804feae74a20d8410a814dac96c
chokowuyu <yoo9077@gmail.com>
2022-01-25 02:54:43 +0900

    Commit

--- vksektm.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('./auto-mpg.csv',header = None)
-
-df.columns = ['mpg','cylinders','displacement','horsepower','weight',
-              'acceleration','model year','origin','name']
-
-df.plot(x='weight',y='mpg', kind = 'scatter')
